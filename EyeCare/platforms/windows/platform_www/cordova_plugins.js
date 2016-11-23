2120b99bdab803b18cd5b37837b539417318d966
Przemek Cichy <przemek.cichy@outlook.com>
2016-11-23 23:44:29 +0000

    Initial Commit

---
+++ EyeCare/platforms/windows/platform_www/cordova_plugins.js
@@ -0,0 +1,55 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/src/windows/DeviceProxy.js",
+        "id": "cordova-plugin-device.DeviceProxy",
+        "pluginId": "cordova-plugin-device",
+        "merges": [
+            ""
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/src/windows/InAppBrowserProxy.js",
+        "id": "cordova-plugin-inappbrowser.InAppBrowserProxy",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "merges": [
+            ""
+        ]
+    },
+    {
+        "file": "plugins/ionic-plugin-keyboard/src/windows/KeyboardProxy.js",
+        "id": "ionic-plugin-keyboard.KeyboardProxy",
+        "pluginId": "ionic-plugin-keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ],
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-device": "1.1.3",
+    "cordova-plugin-inappbrowser": "1.2.1",
+    "cordova-plugin-whitelist": "1.2.2",
+    "ionic-plugin-keyboard": "1.0.9"
+};
+// BOTTOM OF METADATA
+});
---
+++ EyeCare/www/js/Info/Exercises.ts
@@ -0,0 +1,51 @@
+﻿interface ISteps {
+    name: string,
+    shortMessage: string,
+    longMessage: string,
+    duration: number,
+    repeat: number
+}
+
+interface IExercise {
+    name: string,
+    steps: ISteps[],
+    shortMessage: string,
+    longMessage: string,
+    duration: number 
+}
+
+module Exercises{
+    var Exercises: IExercise[];
+
+    class Exercise{
+        public name: string;
+        public steps: ISteps[];
+        public shortMessage: string;
+        public longMessage: string;
+        public duration: number;
+
+        constructor() {
+            //Add to list in memory
+            Exercises = [];
+        }
+
+
+        public SaveToFile(): void{
+
+        }
+
+        public AddToList(): void{
+            var exercise: IExercise = {
+                name: this.name,
+                steps: this.steps,
+                shortMessage: this.shortMessage,
+                longMessage: this.longMessage,
+                duration: this.duration
+            };
+            Exercises.push( exercise );
+        }
+    }
+    function MatchExercise(){
+
+    }
+}
